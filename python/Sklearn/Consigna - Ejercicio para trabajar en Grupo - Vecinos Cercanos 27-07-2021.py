d4742d8802099b1dbf49ca547f94f1db63a2fd8d
Ezquer Guillermo <guillermoezquer@gmail.com>
2022-09-12 10:25:51 -0300

    Cambio de nombre de la carpeta y analisis exploratorio de Social Network

---
+++ python/Sklearn/Consigna - Ejercicio para trabajar en Grupo - Vecinos Cercanos 27-07-2021.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# **Ejercicio para trabajar en Grupo:**
+# 
+# Dado el dataset Breast_cancer_data.csv que se encuentra en Vecinos Cercanos (Knn) --> Código Vecinos Cercanos - Clase.
+# Realizar un entrenamiento y predicción con el algoritmo predictivo Vecinos Cercanos, distancia manhattan y obtener las métricas
+# 
+
+# In[ ]:
+
+
+
+
